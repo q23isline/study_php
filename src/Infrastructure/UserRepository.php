2a116b4859af80888c42c528d79ac4823f422a8e
q23isline <rowpure@gmail.com>
2022-01-10 19:19:48 +0900

    ユーザー取得処理のDB接続設定処理を外に切り出し

--- src/Infrastructure/UserRepository.php
+++ src/Infrastructure/UserRepository.php
@@ -12,18 +12,24 @@ use PDO;
 final class UserRepository
 {
     /**
+     * コンストラクタ
+     *
+     * @param \PDO $pdo PHP データベース接続オブジェクト
+     */
+    public function __construct(
+        private PDO $pdo
+    ) {
+    }
+
+    /**
      * ユーザー情報をすべて取得する
      *
      * @param \App\Model\RoleName $roleName ロール名
      * @param string $name 姓名
      * @return \App\Model\User[] ユーザーオブジェクトの配列
      */
-    public static function findAll(RoleName $roleName, string $name)
+    public function findAll(RoleName $roleName, string $name)
     {
-        // グローバル変数読み込み
-        global $CONF;
-        // プレースホルダの文字列をエスケープさせるため（SQL インジェクション対策） PDO::ATTR_EMULATE_PREPARES を false
-        $pdo = new PDO($CONF['dsn'], $CONF['username'], $CONF['password'], [PDO::ATTR_EMULATE_PREPARES => false]);
         $sql = <<<SQL
 SELECT
     `id`,
@@ -37,7 +43,7 @@ WHERE
 SQL;
 
         // 実行する SQL 文作成
-        $statement = $pdo->prepare($sql);
+        $statement = $this->pdo->prepare($sql);
         $statement->bindValue(':roleName', $roleName->value, PDO::PARAM_STR);
         $statement->bindValue(':name', "%{$name}%", PDO::PARAM_STR);
         // SQL 実行
