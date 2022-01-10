bc11d89232dfe56e082275c626154d41f0e6c3e0
q23isline <rowpure@gmail.com>
2022-01-10 17:12:59 +0900

    コメント文追加

--- src/Infrastructure/UserRepository.php
+++ src/Infrastructure/UserRepository.php
@@ -18,7 +18,9 @@ final class UserRepository
      */
     public static function findAll($params)
     {
+        // グローバル変数読み込み
         global $CONF;
+        // プレースホルダの文字列をエスケープさせるため（SQL インジェクション対策） PDO::ATTR_EMULATE_PREPARES を false
         $pdo = new PDO($CONF['dsn'], $CONF['username'], $CONF['password'], [PDO::ATTR_EMULATE_PREPARES => false]);
         $sql = <<<SQL
 SELECT
@@ -32,9 +34,12 @@ WHERE
     AND `name` LIKE :name;
 SQL;
 
+        // 実行する SQL 文作成
         $statement = $pdo->prepare($sql);
+        // SQL 実行
         $statement->execute($params);
 
+        // SQL 実行結果をオブジェクトにつめて返す
         return $statement->fetchAll(PDO::FETCH_CLASS, 'App\Model\User');
     }
 }
