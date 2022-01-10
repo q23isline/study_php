f80aff5ea69c172e4e35ada479d51521d0e66386
q23isline <rowpure@gmail.com>
2022-01-10 17:22:49 +0900

    不要なユーザープロパティを削除

--- src/Model/User.php
+++ src/Model/User.php
@@ -5,14 +5,7 @@ namespace App\Model;
 
 /**
  * User
- *
- * @property string $id ユーザーID
- * @property string $login_id ログインID
- * @property string $name 姓名
  */
 final class User
 {
-    public string $id;
-    public string $login_id;
-    public string $name;
 }
