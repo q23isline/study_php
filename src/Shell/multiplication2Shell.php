dbc620304e4d760d463b9e9c386bb9d82ef58cb1
rowpure <rowpure@gmail.com>
2022-11-20 01:24:40 +0900

    乗算結果を表示するシェル2

---
+++ src/Shell/multiplication2Shell.php
@@ -0,0 +1,13 @@
+<?php
+
+print_r ( '乗算結果を表示したい数値を入力してください: ' );
+$baseNumber = ( int ) fgets ( STDIN );
+$result = $baseNumber;
+/** 10乗分繰り返す */
+for ( $i = 2; $i <= 10; $i++ )
+{
+
+	$result = $result * $baseNumber;
+	print_r ( "{$baseNumber}^{$i} = {$result}\n" );
+
+}
