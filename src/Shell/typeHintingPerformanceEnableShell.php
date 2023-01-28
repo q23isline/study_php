ec686f61078cbab8997bf36b0bd34054e8f67e4c
rowpure <rowpure@gmail.com>
2023-01-28 13:14:21 +0900

    タイプヒンティング有無の検証コード作成

---
+++ src/Shell/typeHintingPerformanceEnableShell.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+define('MAXIMUM_LOOP_COUNT', 1000000);
+
+$start = hrtime(true);
+
+$character = 'A';
+for ($number = 0; $number < MAXIMUM_LOOP_COUNT; $number++) {
+    getCellAddress($number, $character++);
+}
+
+$end = hrtime(true);
+
+echo '処理時間: ' . ($end - $start) . "ナノ秒\n";
+
+/**
+ * @param int $number
+ * @param string $character
+ * @return string
+ */
+function getCellAddress(int $number, string $character): string
+{
+    return "{$character}{$number}";
+}
